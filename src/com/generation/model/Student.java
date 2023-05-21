07d4c588832b32e179201fad3b07025c2077f3cd
Steven S <ophirian00733@gmail.com>
2023-05-21 15:02:19 -0700

    completed enrollToCourse and isAttendingCourse

--- src/com/generation/model/Student.java
+++ src/com/generation/model/Student.java
@@ -6,47 +6,45 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class Student
-    extends Person
-    implements Evaluation
-{
+public class Student extends Person implements Evaluation {
     private double average;
-
     private final List<Course> courses = new ArrayList<>();
-
     private final Map<String, Course> approvedCourses = new HashMap<>();
 
-    public Student( String id, String name, String email, Date birthDate )
-    {
-        super( id, name, email, birthDate );
+    public Student(String id, String name, String email, Date birthDate) {
+        super(id, name, email, birthDate);
     }
 
-    public void enrollToCourse( Course course )
-    {
-        //TODO implement this method
+    public void enrollToCourse(Course course) {
+        if ( !isAttendingCourse(course.getCode()) ) {
+            courses.add(course);
+            System.out.println("Student enrolled in course: " + course.getCode());
+        } else {
+            System.out.println("Student is already enrolled in this course.");
+        }
     }
 
-    public void registerApprovedCourse( Course course )
-    {
-        approvedCourses.put( course.getCode(), course );
-    }
-
-
-    public boolean isAttendingCourse( String courseCode )
-    {
-        //TODO implement this method
+    public boolean isAttendingCourse(String courseCode) {
+        // Check if the student is attending the course
+        for (Course course : courses) {
+            if (course.getCode().equals(courseCode)) {
+                return true;
+            }
+        }
         return false;
     }
 
+    public void registerApprovedCourse(Course course) {
+        approvedCourses.put(course.getCode(), course);
+    }
+
     @Override
-    public double getAverage()
-    {
+    public double getAverage() {
         return average;
     }
 
     @Override
-    public String toString()
-    {
+    public String toString() {
         return "Student {" + super.toString() + "}";
     }
 }
